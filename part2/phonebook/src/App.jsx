69f265fedf5bbb2405877d36771e6c17404ff7bb
Sayantan Biswas <sayantan251204@gmail.com>
2025-01-18 16:55:59 +0530

    Axios implemented

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567',id:1
-     },
-     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons').then((res)=>{
+          setPersons(res.data);
+    })
+
+  },[])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber]=useState('')
   
